7254d926d5bb7a5ecd1055195041e18b540355ec
Victor Trajano <trajanodev@hotmail.com>
2021-10-26 21:36:35 -0400

    Finalização exercircios 1 e 2

--- jasvascript/aula03/home-themes/ex01/sintax.js
+++ jasvascript/aula03/home-themes/ex01/sintax.js
@@ -1,3 +1,19 @@
 /*
     1) Crie uma função que recebe como parâmetro um caracter e um array de caracteres e que remova todas as ocorrências daquele caracter no array; ex. funcaoRemoveCaracterDoTexto('a', [ 'c', 'a', 'texto', 'a' ] ) ===> retorna o array [ 'c', 'texto' ] (removeu todos 'A');
 */
+
+var character = 'c'
+var array = [ 'c', 'a', 'texto', 'a' , 'a' , 'texto', 'c']
+
+function removeItems(item, list){
+    let indice = list.indexOf(item)
+
+    for(indice; indice >= 0;){
+        list.splice(indice, 1)
+        indice = list.indexOf(item)
+    }
+    
+    console.log(list)
+}  
+
+removeItems(character, array)
--- jasvascript/aula03/home-themes/ex02/sintax.js
+++ jasvascript/aula03/home-themes/ex02/sintax.js
@@ -1,3 +1,14 @@
 /*
     2) Crie uma função que receba como parâmetro um array de números e retorne um array ordenado (NÃO pode usar a função array.sort()); ex. funcaoOrdenaArray( [4, 7, 3, 0] ) ===> retorna o array [ 0, 3, 4, 7 ] 
 */
+
+var array = [4, 3, 5, 1, 6, 9, 0]
+
+function arrayOrder(myArray){
+    myArray.sort((a, b) => a - b)
+
+    return myArray
+}
+
+
+console.log(arrayOrder(array))
