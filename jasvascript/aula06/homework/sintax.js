aed45572d72d1d86721fc7cd23a8f602a93cded3
Victor Trajano <trajanodev@hotmail.com>
2021-10-31 18:00:50 -0400

    Finalizado até o menu 4 - desalocação de colaborador

--- jasvascript/aula06/homework/sintax.js
+++ jasvascript/aula06/homework/sintax.js
@@ -5,112 +5,144 @@
 //Variaveis
 const MENSAGEM_DE_ERRO_NOME = "É preciso informar um nome para o valor em questão";
 const MENSAGEM_DE_ERRO_NÚMERO = "É preciso informar um número para o valor em questão";
-var id = 0;
 var code = 0
+var id = 0
 var arrayOfContributors = [];
-var arrayOfClocking = [];
 var arrayOfProjects = [];
 
 //Validações
 
-class Validations{
+class Validations {
     isNumber = (value) => {
-        if(typeof value === 'number' && !isNaN(value)){
+        if (typeof value === 'number' && !isNaN(value)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     isProjectExists = (value) => {
-        if(this.isNumber(value) && arrayOfProjects.indexOf(value) !== -1){
+        if (this.isNumber(value) && arrayOfProjects.indexOf(value) !== -1) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 }
-
 //Criação das classes
 
-class Marcacao{
+class Marcacao {
     day = '';//MENSAGEM_DE_ERRO_NÚMERO;
     hour = '';//MENSAGEM_DE_ERRO_NÚMERO;
 
-    constructor(day, hour){
+    constructor(day, hour) {
         this.day = new Validations().isNumber(day) ? day : this.day;
         this.hour = new Validations().isNumber(hour) ? hour : this.hour;
     }
 }
 
-class Colaborador{
+class Colaborador {
     id = 0;
-    name = MENSAGEM_DE_ERRO_NOME;
-    projectCode = 1;
+    name = '';
     clocking = [];
 
-    constructor(name){
+    constructor(name) {
         this.name = name;
+        this.id = id += 1
     }
 
-    pointRegister = (d, h) =>{
-        this.clocking.push({    
+    registrarContribuidor = () => {
+        contribuitorRegister();
+    }
+
+    pointRegister = (d, h) => {
+        this.clocking.push({
             day: d,
             hour: h
         })
-    } 
-
-    contribuitorRegister = (contribuitor) => {
-        arrayOfContributors.push({
-            id: arrayOfContributors.indexOf(id) === -1 ? id += 1 : contribuitor.id,
-            name: contribuitor.name,
-            projectCode: contribuitor.projectCode,
-            clocking: this.clocking       
-        })
     }
 }
 
-console.log(arrayOfContributors);
-const contribuitor1 = new Colaborador('Joãozinho');
-contribuitor1.contribuitorRegister(contribuitor1);
-contribuitor1.pointRegister(1, 8);
+const contribuitorRegister = (name) => {
+    let contribuitor = new Colaborador(name);
+    if (arrayOfContributors.indexOf(contribuitor.id) === -1) {
+        arrayOfContributors.push(contribuitor);
+        
+    }
+}
 
-const contribuitor2 = new Colaborador('Pelé');
-contribuitor2.contribuitorRegister(contribuitor2)
-
-
-class Project{
-    code = 1; //MENSAGEM_DE_ERRO_NUMERO
+class Project {
+    code = null; //MENSAGEM_DE_ERRO_NUMERO
     title = '';//MENSAGEM_DE_ERRO_NOME
     allocatedContribuitors = [];
 
-    constructor(title){
-        this.title = title
+    constructor(title) {
+        this.title = title;
+        this.code = code += 1;
     }
 
-    projectRegister = (project) => {
-        arrayOfProjects.push({
-            code: project.code,
-            title: project.title,
-            allocatedContribuitors: this.allocatedContribuitors
-        })
-    }
-
-    allocateContribuitorOnProject(contribuitor){
-        if(new Colaborador(contribuitor1).projectCode === this.code){
-            this.allocatedContribuitors.push(contribuitor)  
-        }
+    registroDeProjeto = () => {
+        projectRegister();
     }
 }
 
-console.log(arrayOfProjects)
-const projeto1 = new Project('Projeto1')
-projeto1.projectRegister(projeto1)
-projeto1.allocateContribuitorOnProject(contribuitor1)
-projeto1.allocateContribuitorOnProject(contribuitor2)
+const projectRegister = (title) => {
+    let project = new Project(title);
 
-const projeto2 = new Project('Projeto2')
-projeto2.projectRegister(projeto2)
-projeto2.allocateContribuitorOnProject(contribuitor2)
+    if (arrayOfProjects.indexOf(project.id) === -1) {
+        arrayOfProjects.push(project);
+    }
+    console.log(arrayOfProjects);
+}
 
+const allocateContribuitorsOnProject = (contribuitor, project) => {
+    
+    let findContribuitor = arrayOfContributors.find(c => c.name === contribuitor);
+    let findProject = arrayOfProjects.find(p => p.title === project);
+    findProject.allocatedContribuitors.push(findContribuitor);
+    console.log(arrayOfProjects);
+}
 
+const dislocateCollaborator = (project, contribuitor) =>{
+    let findProject = arrayOfProjects.find(p => p.title === project);
+    let index = findProject.allocatedContribuitors.indexOf(contribuitor);
+    findProject.allocatedContribuitors.splice(index, 1);
+}
+
+const showMenu = () => {
+    var menu = prompt('Escolha um numero');
+
+    var codeMenu = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    if (codeMenu.indexOf(menu) === -1) {
+        alert('Você deve inserir um dos números do menu para continuar ou escolha a opção 9 para sair do sistema');
+
+    } else {
+
+        switch (menu) {
+            case '1': 
+                let nome = prompt('Qual nome do colaborador que deseja cadastrar ?');
+                contribuitorRegister(nome);
+                console.log(arrayOfContributors);
+                showMenu();
+            break;
+            case '2':
+                let nomeDoProjeto = prompt('Qual nome do projeto que desejas cadastrar?');
+                projectRegister(nomeDoProjeto);
+                showMenu();
+                break;
+            case '3':
+                let contribuitor = prompt('Digite um colaborador');
+                let project = prompt(`Digite o nome do projeto que você deseja alocar ${contribuitor}`);
+                allocateContribuitorsOnProject(contribuitor, project);
+                showMenu();
+                break;
+            case '4': 
+                let nomeProjeto = prompt('Nome do projeto');
+                let empregado = prompt('Nome do empregado');
+                dislocateCollaborator(nomeProjeto, empregado);
+            return
+        }
+    }
+}
+showMenu()
