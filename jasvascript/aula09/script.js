da1e5168a210f209beebe77f5127fb6e3ffa40be
Victor Trajano <trajanodev@hotmail.com>
2021-11-05 11:28:12 -0400

    Finalizado 90%

--- jasvascript/aula09/script.js
+++ jasvascript/aula09/script.js
@@ -1,4 +1,3 @@
-// const { default: axios } = require("axios");
 
 //#region Validação Email
 const validarEmail = () => {
@@ -113,14 +112,52 @@ const irPara = (origem, destino) => {
     elementoOrigem.className = elementoOrigem.className.replace('d-flex', 'd-none');
 }
 
+const irParaEdicao = (userEdit) => {
+    let elementoOrigem = document.getElementById('home');
+    let elementoDestino = document.getElementById('edition');
+    elementoDestino.className = elementoDestino.className.replace('d-none', 'd-flex');
+    elementoOrigem.className = elementoOrigem.className.replace('d-flex', 'd-none');
+
+    let id = userEdit.target.id
+    let nameInput = document.getElementById('name-input-edit')
+    let dateInput = document.getElementById('date-input-edit')
+    let emailInput = document.getElementById('email-input-edit')
+    let senhaInput = document.getElementById('password-input-edit')
+
+    axios.get(`http://localhost:3000/colaboradores/${id}`)
+        .then(response => {
+            const res = response.data
+            nameInput.value = res.name
+            dateInput.value = res.dataNascimento
+            emailInput.value = res.email
+            senhaInput.value = res.senha
+            console.log(response.data)
+        })
+}
+
+const atualizarUsuario = () => {
+    
+    let nameInput = document.getElementById('name-input-edit')
+    let dateInput = document.getElementById('date-input-edit')
+    let emailInput = document.getElementById('email-input-edit')
+    let senhaInput = document.getElementById('password-input-edit')
+
+    let colaboradorAtualizado = {
+       name: nameInput.value,
+       dataNascimento: dateInput.value,
+       email: emailInput.value,
+       senha: senhaInput.value,
+    }
+
+    axios.put(`http://localhost:3000/colaboradores`, colaboradorAtualizado)
+}
+
 const validarLogin = () => {
     axios.get('http://localhost:3000/colaboradores')
         .then(response => {
             let emailDigitado = document.getElementById('email-input-login').value;
             let senhaDigitada = document.getElementById('password-input-login').value;
-
             let podeLogar = response.data.some(c => c.email === emailDigitado && c.senha === senhaDigitada);
-
             if (podeLogar) {
                 irPara('login', 'home');
             }
@@ -133,14 +170,14 @@ const validarNome = () => {
     let ehValido = true;
     let nameInputValue = document.getElementById('name-input-registration').value;
     let arrayName = [...nameInputValue]
-    arrayName = arrayName.filter(e =>  e !== ' ')
+    arrayName = arrayName.filter(e => e !== ' ')
     let possuiNumero = arrayName.some(n => isNaN(parseInt(n)) === false && n !== ' ')
     let temCharEspecial = arrayName.some(c => c.toLowerCase() === c.toUpperCase() && isNaN(parseInt(c)))
-   
-    if(possuiNumero){
+
+    if (possuiNumero) {
         ehValido = false;
     }
-    if(temCharEspecial){
+    if (temCharEspecial) {
         ehValido = false;
     }
 
@@ -158,15 +195,25 @@ const listarUsuarios = () => {
             let res = response.data
             res.forEach(res => {
                 let userList = document.getElementById('user-list')
+                let myDiv = document.createElement('div')
                 let li = document.createElement('li')
+                li.innerText = res.email
+
                 let buttonUp = document.createElement('button')
                 buttonUp.innerText = 'Update'
+                buttonUp.setAttribute('id', res.id)
+                buttonUp.setAttribute('classs' , 'button-id')
+                buttonUp.addEventListener('click', irParaEdicao)
+
                 let buttonDel = document.createElement('button')
                 buttonDel.innerText = 'Delete'
-                li.innerText = res.email
-                userList.appendChild(li)
-                userList.appendChild(buttonUp)
-                userList.appendChild(buttonDel)
+                buttonDel.setAttribute('id', res.id)
+                buttonDel.addEventListener('click', deleteUser)
+
+                myDiv.appendChild(li)
+                myDiv.appendChild(buttonUp)
+                myDiv.appendChild(buttonDel)
+                userList.appendChild(myDiv)
             })
         })
 };
@@ -174,21 +221,29 @@ const listarUsuarios = () => {
 const listarUsuariosPorNome = () => {
     let getInputValue = document.getElementById('inputSearch').value
     axios.get('http://localhost:3000/colaboradores')
-    .then((response) => {
-        let res = response.data
-        console.log(res)
-        let usuarioEncontrado = res.find(userEmail => userEmail.email === getInputValue)
-        let userList = document.getElementById('user-list')
-        let li = document.createElement('li')
-        li.innerText = usuarioEncontrado.email
-        userList.appendChild(li)
-        console.log(usuarioEncontrado)
-    })
-    .catch(err => {
-        throw new Error('Você nao inseriu email' + err)
-    })
+        .then((response) => {
+            let res = response.data
+            console.log(res)
+            let usuarioEncontrado = res.find(userEmail => userEmail.email === getInputValue)
+            let userList = document.getElementById('user-list')
+            let li = document.createElement('li')
+            li.innerText = usuarioEncontrado.email
+            userList.appendChild(li)
+            console.log(usuarioEncontrado)
+        })
+        .catch(err => {
+            throw new Error('Você nao inseriu email' + err)
+        })
 }
 
+const validarAtualizacao = () => {  
+    let atualizacaoValida = validarData() && validarEmail() && validarSenha();
+    console.log(`Cadastro ${cadastroValido ? 'válido!' : 'inválido'}`);
+
+    if (atualizacaoValida) {
+        atualizarUsuario();
+    }
+}
 
 const validarCadastro = () => {
     let cadastroValido = validarData() && validarEmail() && validarSenha();
@@ -200,7 +255,7 @@ const validarCadastro = () => {
 }
 
 const cadastrarUsuario = () => {
-    let nameInput
+    let nameInput = document.getElementById('name-input-registration')
     let dataInput = document.getElementById('date-input-registration');
     let emailInput = document.getElementById('email-input-registration');
     let senhaInput = document.getElementById('password-input-registration');
@@ -208,6 +263,7 @@ const cadastrarUsuario = () => {
     // aqui entra lógica de POST para salvar usuário
 
     let colaborador = {
+        name: nameInput.value,
         email: emailInput.value,
         senha: senhaInput.value,
         dataNascimento: dataInput.value
@@ -224,3 +280,11 @@ const cadastrarUsuario = () => {
             console.log('Erro => ', error);
         });
 };
+
+const deleteUser = (user) => {
+    let id = user.target.id
+    console.log(id)
+
+    axios.delete(`http://localhost:3000/colaboradores/${id}`)
+    console.log('algo de errado nao esta certo')
+}
